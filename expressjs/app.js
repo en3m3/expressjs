9b78ff6e5be289ce165394d4a04e5ece94892230
en3m3 <71306799+en3m3@users.noreply.github.com>
2022-01-15 01:45:13 -0700

    test

---
+++ expressjs/app.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+
+const app = express();
+
+
+app.use('/users', (req, res, next) => {
+    console.log('/users Middleware');
+    res.send('<p>Middleware that handles /users</p>')
+});
+
+app.use('/', (req, res, next) => {
+    console.log('/ Middleware');
+    res.send('<p>Middleware that handles /</p>')
+    next();
+});
+
+app.listen(5000);
